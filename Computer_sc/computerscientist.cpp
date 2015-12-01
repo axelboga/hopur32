cb85126b13ef5a3ab82d8e2f1317a460689e1430
hafdisinga <hafdisa15@ru.is>
2015-12-01 18:47:58 +0000

    laga search

--- Computer_sc/computerscientist.cpp
+++ Computer_sc/computerscientist.cpp
@@ -59,7 +59,11 @@ void ComputerScientist::setYearOfDeath(string d_year) {
 }
 
 ostream& operator <<(ostream& outs, ComputerScientist& c) { //how to output the object computerScientist
-    outs << left << setw(11) << c.firstName << left << setw(12) << c.lastName << left << setw(14)
+    string tempF = c.getFirstName();
+    tempF[0] = toupper(tempF[0]);
+    string tempL = c.getLastName();
+    tempL[0] = toupper(tempL[0]);
+    outs << left << setw(11) << tempF << left << setw(12) << tempL << left << setw(14)
          << c.sex << left << setw(16)<< c.yearOfBirth << left <<setw(4) << c.yearOfDeath << endl;
     return outs;
 }

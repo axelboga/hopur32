1bea45e02b9a951cc2b3e87e8c59c23a9407f88b
hafdisinga <hafdisa15@ru.is>
2015-12-03 10:57:34 +0000

    set og get föll

--- Computer_sc/computer.cpp
+++ Computer_sc/computer.cpp
@@ -5,3 +5,31 @@ Computer::Computer()
 
 }
 
+int Computer::getId() const {
+    return id;
+}
+
+
+void Computer::setId(int my_id) {
+    id = my_id;
+}
+
+
+string Computer::getName() const {
+    return name;
+}
+
+
+void Computer::setName(string my_name) {
+    name = my_name;
+}
+
+
+string Computer::getYear() const {
+    return year;
+}
+
+
+void Computer::setYear(string my_year) {
+    year = my_year;
+}
